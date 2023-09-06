1b4005008422e3eb36fcb73d6eff87d0446eac6d
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-09-06 20:42:10 -0300

    initial config / first commit

---
+++ app/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+export default function Search() {
+  return (
+    <div>
+      <input className="before:content" type="text" placeholder="Search" />
+    </div>
+  );
+}
