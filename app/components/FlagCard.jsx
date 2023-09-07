7541423a145f553cf3810f2414fe2f4f616b0b71
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-09-07 11:11:32 -0300

    home card layout responsive

--- app/components/FlagCard.jsx
+++ app/components/FlagCard.jsx
@@ -5,32 +5,35 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
-export default function FlagCard() {
+export default function FlagCard(props) {
   return (
-    <div className="flex justify-center">
-      <Card id="theme" sx={{ maxWidth: 320 }}>
-        <CardActionArea>
-          <CardMedia component="img" image="https://flagcdn.com/w320/br.png" />
-          <CardContent className="pb-12 ml-2">
-            <Typography gutterBottom variant="h5" component="div">
-              Brazil
-            </Typography>
-            <Typography variant="body3">
-              <ul>
-                <li>
-                  <span className="font-bold">Population:</span> 8,797,450
-                </li>
-                <li>
-                  <span className="font-bold">Region:</span> America
-                </li>
-                <li>
-                  <span className="font-bold">Capital:</span> Brasilia
-                </li>
-              </ul>
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
+    <Card id="theme" className="w-[320px]">
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          className="h-[160px]"
+          image={props.flags.png}
+        />
+        <CardContent className="pb-12 ml-2">
+          <Typography gutterBottom variant="h5" component="div">
+            {props.name.common}
+          </Typography>
+          <Typography variant="body3">
+            <ul>
+              <li>
+                <span className="font-bold">Population:</span>{" "}
+                {props.population}
+              </li>
+              <li>
+                <span className="font-bold">Region:</span> {props.region}
+              </li>
+              <li>
+                <span className="font-bold">Capital:</span> {props.capital}
+              </li>
+            </ul>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,21 @@ import Filter from "./components/Filter";
 import FlagCard from "./components/FlagCard";
 import Search from "./components/Search";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+
+  const countryElements = await data.map((country) => {
+    return <FlagCard {...country} />;
+  });
+
   return (
     <main>
       <Search />
       <Filter />
-      <FlagCard />
+      <div className="flex flex-wrap gap-12 justify-center mb-6">
+        {countryElements}
+      </div>
     </main>
   );
 }
